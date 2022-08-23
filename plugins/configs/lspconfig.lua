6660ec3e5497fa3630994be5aac34c59553cc588
nulloneguy <nulloneguy@ya.ru>
2022-08-23 22:14:31 +0700

    Update lspconfig.lua

--- plugins/configs/lspconfig.lua
+++ plugins/configs/lspconfig.lua
@@ -10,3 +10,41 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.volar.setup {
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+  documentFeatures = {
+    documentColor = false,
+    documentSymbol = true,
+    foldingRange = true,
+    linkedEditingRange = true,
+    selectionRange = true,
+  },
+  languageFeatures = {
+    callHierarchy = true,
+    codeAction = true,
+    codeLens = true,
+    completion = {
+      defaultAttrNameCase = "kebabCase",
+      defaultTagNameCase = "both",
+    },
+    definition = true,
+    diagnostics = true,
+    documentHighlight = true,
+    documentLink = true,
+    hover = true,
+    implementation = true,
+    references = true,
+    rename = true,
+    renameFileRefactoring = true,
+    schemaRequestService = true,
+    semanticTokens = false,
+    signatureHelp = true,
+    typeDefinition = true,
+  },
+  typescript = {
+    serverPath = "",
+  },
+}
+
+lspconfig.vuels.setup {}
