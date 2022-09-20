5da6c469ce5b43e5e9568729c81dfceaafd72137
01GUY <87767572+nulloneguy@users.noreply.github.com>
2022-09-20 18:15:33 +0700

    Update null-ls.lua

--- plugins/configs/null-ls.lua
+++ plugins/configs/null-ls.lua
@@ -10,13 +10,12 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "vue" } },
+  b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "scss", "vue" } },
 
   -- Lua
   b.formatting.stylua,
 
   -- Shell
-  b.formatting.shfmt,
   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
 }
 
